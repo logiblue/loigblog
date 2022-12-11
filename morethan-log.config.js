4fc26a1d6e84071e6126d0ab7317dba8a4a8cf2d
logiblue <karanikolasms@gmail.com>
2022-12-11 17:30:06 +0200

    init

---
+++ morethan-log.config.js
@@ -0,0 +1,64 @@
+const CONFIG = {
+  // profile setting
+  profile: {
+    name: 'logibloh',
+    image: '/kostas.jpg',
+    discription: 'frontend developer',
+    linkedin: 'karanikolasms',
+    email: 'karanikolasms@gmail.com',
+    github: 'logiblue',
+    instagram: 'konstantinos.krn',
+  },
+  projects: [
+    {
+      name: 'Untilled',
+      href: 'https://untilled.web.app'
+    }
+  ],
+  // blog setting
+  blog: {
+    title: 'logiblog',
+    description: 'welcome to morethan-log!',
+  },
+
+  // CONFIG configration
+  link: 'https://morethan-log.vercel.app',
+  since: 2022, // If leave this empty, current year will be used.
+  lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  postsPerPage: 10,
+  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ['Blog', 'Website', 'Notion'],
+  },
+
+  // notion configuration
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+    // if you prefer not to make your database public, use this!!!
+    accessToken: process.env.NOTION_ACCESS_TOKEN
+  },
+
+  // plugin configuration
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: '' // ex. G-9N3FE0117Q
+    }
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: '' // ex. qvdR1gXMirk_DCUOKPgRnxu2x6fqSPrquYnEZZMjR9w
+    }
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: 'logiblue/logiblog',
+      'issue-term': 'og:title',
+      label: '💬Utterances',
+    }
+  },
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+module.exports = CONFIG
---
+++ src/components/Layout/Header/index.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+import CONFIG from 'morethan-log.config'
+import NavBar from './NavBar'
+
+const Header = ({ fullWidth }) => {
+  const navRef = useRef(null)
+  const [, setTheme] = useState()
+
+  useEffect(() => {
+    if (typeof window === 'object') {
+      setTheme(localStorage.theme || 'light')
+    }
+  }, [])
+
+  // const handleClick = () => {
+  //   const changedTheme = localStorage.theme !== 'dark' ? 'dark' : 'light'
+  //   localStorage.setItem('theme', changedTheme)
+  //   setTheme(changedTheme)
+  //   changedTheme === 'dark'
+  //     ? document.documentElement.classList.add('dark')
+  //     : document.documentElement.classList.remove('dark')
+  // }
+
+  return (
+    <>
+      <div
+        className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-6 py-8 bg-opacity-60 max-w-6xl px-4 ${
+          fullWidth && 'px-4 md:px-24'
+        }`}
+        id="sticky-nav"
+        ref={navRef}
+      >
+        <Link href="/">
+          <a aria-label={CONFIG.blog.title}>
+            <div className="flex items-center">
+              <div className="ml-2 text-black dark:text-white header-name">
+                {CONFIG.blog.title}
+              </div>
+            </div>
+          </a>
+        </Link>
+        <div className={`flex gap-3 items-center`}>
+          {/* <div onClick={handleClick}>{theme}</div> */}
+          <NavBar />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
